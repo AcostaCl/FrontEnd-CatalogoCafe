df5e5705801ebb5df4f56545b7e88b62e53f1102
Celina <idkmahee@gmail.com>
2025-09-18 12:50:57 -0300

    agregamos un maquetado de paginacion al final de la tabla del administrador

--- src/components/pages/Administrador.jsx
+++ src/components/pages/Administrador.jsx
@@ -1,21 +1,25 @@
-import { Table } from "react-bootstrap";
+import { Table, Button } from "react-bootstrap";
 import ItemProducto from "./producto/ItemProducto";
 import { Link } from "react-router";
 import { useEffect, useState } from "react";
-import { leerProductos } from "../../helpers/queries";
+import { leerProductosPaginados } from "../../helpers/queries";
 
 const Administrador = () => {
   const [listaProductos, setListaProductos] = useState([]);
+  const [page, setPage] = useState(1); //número de página actual
+  const [limit] = useState(10); //cantidad de productos por página (fijo en 10).
+  const [totalPages, setTotalPages] = useState(1); //total de páginas disponibles (lo devuelve el backend).
 
   useEffect(() => {
     obtenerProductos();
-  }, []);
+  }, [page]);
 
   const obtenerProductos = async () => {
-    const respuesta = await leerProductos();
+    const respuesta = await leerProductosPaginados(page, limit);
     if (respuesta.status === 200) {
       const datos = await respuesta.json();
-      setListaProductos(datos);
+      setListaProductos(datos.productos);
+      setTotalPages(datos.totalPages);
     } else {
       console.info("Ocurrio un error al buscar los productos");
     }
@@ -48,12 +52,38 @@ const Administrador = () => {
             <ItemProducto
               key={producto._id}
               producto={producto}
-              fila={indice + 1}
+              fila={(page - 1) * limit + indice + 1}
               setListaProductos={setListaProductos}
+              page={page}
+              limit={limit}
             ></ItemProducto>
           ))}
+          {/* 
+          (page - 1) * limit + indice + 1 
+          (1-1) * 10 + 0 + 1 = 1
+          (2-1) *10 + 0 + 1= 
+          */}
         </tbody>
       </Table>
+      <div className="d-flex justify-content-center align-items-center my-3">
+        <Button
+          variant="secondary"
+          onClick={() => setPage((prev) => Math.max(prev - 1, 1))}
+          disabled={page === 1}
+        >
+          Anterior
+        </Button>
+        <span className="mx-3">
+          Página {page} de {totalPages}
+        </span>
+        <Button
+          variant="secondary"
+          onClick={() => setPage((prev) => Math.min(prev + 1, totalPages))}
+          disabled={page === totalPages}
+        >
+          Siguiente
+        </Button>
+      </div>
     </section>
   );
 };
